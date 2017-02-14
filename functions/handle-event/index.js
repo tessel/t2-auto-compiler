958854e457ca824b4c784ca960ad352de4ea7cd0
Francis Gulotta <wizard@roborooter.com>
2017-02-13 23:24:06 -0500

    restore the lambda scanner

--- functions/handle-event/index.js
+++ functions/handle-event/index.js
@@ -1,8 +1,21 @@
 import Promise from 'bluebird'
-import timer from '../../lib/events/timer'
+import { listener } from '../../lib/listener'
+import { publish as snsPublish } from '../../lib/sns'
+import { scanTimeout } from '../../lib/config'
+import { tarballUrl } from '../../lib/registry'
+import { savePackageInfo } from '../../lib/pkgs'
+
+export function handler (event, context, callback) {
+  Promise
+    .resolve(event.Records)
+    .map(processEvent, { concurrency: 1 })
+    .then(() => undefined)
+    .asCallback(callback)
+}
 
 const events = {
-  timer
+  timer: scanAndPublish,
+  detect: savePackageInfo
 }
 
 function processEvent (record) {
@@ -10,14 +23,31 @@ function processEvent (record) {
   if (typeof events[data.type] === 'function') {
     return events[data.type](data)
   } else {
-    console.warn(`Unknown event type ${data.type}`, record.Sns.Message)
+    console.error(`Unknown event type ${data.type}`, record.Sns.Message)
   }
 }
 
-export function handler (event, context, callback) {
-  Promise
-    .resolve(event.Records)
-    .map(processEvent, { concurrency: 2 })
-    .then(() => undefined)
-    .asCallback(callback)
+async function scanAndPublish (data) {
+  const stream = await listener(newPackage)
+  console.log(`Processing NPM registry Stream from ${stream.sequence()}`)
+  await Promise.delay(scanTimeout)
+  console.log(`Quiting after ${scanTimeout / 1000} seconds`)
+  console.log(`Processed NPM registry Stream to ${stream.sequence()}`)
+  stream.end()
+}
+
+function newPackage (newPackage) {
+  const doc = newPackage.doc
+  const { name, versions } = doc
+  if (!name || typeof versions !== 'object') {
+    return
+  }
+
+  return Promise.map(Object.keys(versions), async version => {
+    const url = await tarballUrl(name, version, doc)
+    return snsPublish({
+      type: 'detect',
+      data: { name, version, url }
+    })
+  })
 }
--- functions/scan-registry/index.js
+++ functions/scan-registry/index.js
@@ -1,54 +1,19 @@
 import Promise from 'bluebird'
 import { listener } from '../../lib/listener'
-import { packages } from '../../lib/db'
-import { tarballUrl, checkForGypFileRetry } from '../../lib/registry'
-
-// export function published (event) {
-//   console.log(`Analyzing ${event.data.name}@${event.data.version}`)
-//   packages.get(event.data.name, event.data.version).then(info => {
-//     if (info && typeof info.gypFile === 'boolean') {
-//       console.log(`${info.name}@${info.version} has a gypFile value of ${info.gypFile}`)
-//       return
-//     }
-//   })
-// }
-
-function saveInformation (doc) {
-  const versions = Object.keys(doc.versions)
-
-  return Promise.map(versions, async version => {
-    const url = tarballUrl(doc, version)
-    const { name } = doc
-    let gypFile
-    try {
-      gypFile = await checkForGypFileRetry(url)
-    } catch (e) {
-      console.error('Error checking', name)
-      console.error(e)
-      return
-    }
-    const data = {
-      name,
-      version,
-      gypFile,
-      url
-    }
-
-    return Promise.all([
-      packages.set(`all/${name}/${name}@${version}`, data),
-      packages.set(`all/${name}/${version}`, data),
-      gypFile && packages.set(`gypFile/${name}@${version}`, data),
-      gypFile && packages.set(`gypFile/${name}/${name}@${version}`, data)
-    ])
-  })
-}
+import { savePackageInfo } from '../../lib/pkgs'
+import { tarballUrl } from '../../lib/registry'
 
 function newPackage (newPackage) {
   const doc = newPackage.doc
-  if (!doc.name || typeof doc.versions !== 'object') {
+  const { name, versions } = doc
+  if (!name || typeof versions !== 'object') {
     return
   }
-  return saveInformation(doc)
+
+  return Promise.map(Object.keys(versions), async version => {
+    const url = await tarballUrl(name, version, doc)
+    return savePackageInfo({ name, version, url })
+  })
 }
 
 const listen = () => listener(newPackage).catch(e => {
